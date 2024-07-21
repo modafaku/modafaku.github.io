921f471f4ee075efa9de791e389620c3bd3443a0
modafaku <modafaku@gmail.com>
2024-07-20 23:42:14 -0400

    migrate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,20 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
 import astroGhostCMS from '@matthiesenxyz/astro-ghostcms';
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
-  output: 'server',
+  site: 'https://modafaku.github.io',
+//  output: 'server',
   integrations: [astroGhostCMS({
-    ghostURL: "https://ghost.modafaku.com",
+    ghostURL: 'https://ghost.modafaku.com',
     verbose: true,
     ThemeProvider: {
-      disableThemeProvider: true
+      disableThemeProvider: true,
     }
-  }), mdx(), sitemap(), solidJs(), tailwind({
-    applyBaseStyles: false
-  })],
-  adapter: node({
-    mode: "standalone"
-  })
-});
+  }), mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+})
